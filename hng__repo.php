9cef3d8de818fe01d4f0fb05beb13a9520802792
Elizabeth Okoro <elizabethokorodavid@gmail.com>
2018-05-10 20:55:23 +0100

    first commit

---
+++ hng__repo.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>WELCOME TO HNG INTERNSHIP 4.0!</title>
+<style>
+body{
+ text-align: center;
+ margin-top: 20%;
+}
+ul{
+ list-style-type: none;
+ padding: 0;
+}
+h6 {
+  position: relative;
+  left: 100px;
+  top: 150px;
+  align-items: end;
+}
+h1{text-align:center;
+}
+p {text-align:center;
+color: #2F80ED;
+font-style:italic;}
+body {background-color: #7E114C;
+color: white;}
+</style>
+</head>
+<body>
+
+<h1>WELCOME TO HNG INTERNSHIP 4.0</h1>
+     <?php
+     date_default_timezone_set("Africa/Lagos"); //this sets the time_zone
+     echo date("l"). " "; //this sets day
+     echo date("d.m.y") . " " ; //this sets date
+     echo date("h:i:sA") ; //this sets time
+ ?>
+<p> Bringing programming to your finger tips</p>
+<ul><li>HTML<li>CSS<li>JAVASCRIPT<li>GIT<li>PHP</ul>
+<button><a href="https://www.codecademy.com/">help</a></button>
+
+
+</body>
+</html>
